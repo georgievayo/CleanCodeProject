532070fbf2e6eeb828b6bbb5eccfe0a47fe7b154
georgievayo <yo.ana.georgieva@abv.bg>
2018-06-18 11:18:25 +0300

    Implement tests for all methods of Unit of work.

---
+++ FindAndBook.API/FindAndBook.Tests/Data/UnitOfWorkTests.cs
@@ -0,0 +1,35 @@
+﻿using FindAndBook.Data;
+using FindAndBook.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace FindAndBook.Tests.Data
+{
+    [TestFixture]
+    public class UnitOfWorkTests
+    {
+        [Test]
+        public void CommitShould_CallDbContextSaveChanges()
+        {
+            var mockedDbContext = new Mock<IDbContext>();
+
+            var unitOfWork = new UnitOfWork(mockedDbContext.Object);
+
+            unitOfWork.Commit();
+
+            mockedDbContext.Verify(c => c.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public void CommitAsyncShould_CallDbContextSaveChangesAsync()
+        {
+            var mockedDbContext = new Mock<IDbContext>();
+
+            var unitOfWork = new UnitOfWork(mockedDbContext.Object);
+
+            unitOfWork.CommitAsync();
+
+            mockedDbContext.Verify(c => c.SaveChangesAsync(), Times.Once);
+        }
+    }
+}
