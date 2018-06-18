ccf72ba05ac314465993580ba630331182580e05
georgievayo <yo.ana.georgieva@abv.bg>
2018-06-18 09:37:42 +0300

    Add project template and db models.

---
+++ FindAndBook.API/FindAndBook.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FindAndBook.API.WebApiApplication" Language="C#" %>
---
+++ FindAndBook.API/FindAndBook.Models/User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+
+namespace FindAndBook.Models
+{
+    public class User : IdentityUser, IUser
+    {
+        public User()
+        {
+            this.Bookings = new HashSet<Booking>();
+            this.Restaurants = new HashSet<Restaurant>();
+        }
+
+        public User(string username, string email, string firstName, string lastName, string phoneNumber)
+            : this()
+        {
+            this.UserName = username;
+            this.Email = email;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.PhoneNumber = phoneNumber;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public virtual ICollection<Booking> Bookings { get; set; }
+
+        public virtual ICollection<Restaurant> Restaurants { get; set; }
+    }
+}
