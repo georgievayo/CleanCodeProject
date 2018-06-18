042241840fd3815fcf18983a0537f64b4c16cf53
georgievayo <yo.ana.georgieva@abv.bg>
2018-06-18 10:09:15 +0300

    Implement db context and abstraction.

---
+++ FindAndBook.API/FindAndBook.Data/DbContext.cs
@@ -0,0 +1,83 @@
+﻿using FindAndBook.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace FindAndBook.Data
+{
+    public partial class DbContext : IdentityDbContext<User>
+    {
+        public DbContext()
+            : base("FindAndBook", throwIfV1Schema: false)
+        {
+            Database.SetInitializer<DbContext>(null);
+        }
+
+        public static DbContext Create()
+        {
+            return new DbContext();
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public DbSet<Table> Tables { get; set; }
+
+        public DbSet<BookedTables> BookedTables { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Booking>()
+                .Property(b => b.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<Booking>()
+                .HasMany(x => x.Tables)
+                .WithRequired(x => x.Booking)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Restaurant>()
+                .HasMany(x => x.Bookings)
+                .WithRequired(x => x.Restaurant)
+                .WillCascadeOnDelete();
+            modelBuilder.Entity<Restaurant>()
+                .HasMany(x => x.Tables)
+                .WithRequired(x => x.Restaurant)
+                .WillCascadeOnDelete();
+            modelBuilder.Entity<Restaurant>()
+                .HasRequired(x => x.Manager)
+                .WithMany(x => x.Restaurants)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Table>()
+                .Property(x => x.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public IDbSet<TEntity> DbSet<TEntity>() where TEntity : class
+        {
+            return this.Set<TEntity>();
+        }
+
+        public void SetAdded<TEntry>(TEntry entity) where TEntry : class
+        {
+            var entry = this.Entry(entity);
+            entry.State = EntityState.Added;
+        }
+
+        public void SetDeleted<TEntry>(TEntry entity) where TEntry : class
+        {
+            var entry = this.Entry(entity);
+            entry.State = EntityState.Deleted;
+        }
+
+        public void SetUpdated<TEntry>(TEntry entity) where TEntry : class
+        {
+            var entry = this.Entry(entity);
+            entry.State = EntityState.Modified;
+        }
+    }
+}
