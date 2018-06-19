b73ab22d00794003581004fce783280f8f7e30b7
georgievayo <yo.ana.georgieva@abv.bg>
2018-06-19 13:12:41 +0300

    Setup autofac IoC container.

---
+++ FindAndBook.API/FindAndBook.API/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Autofac.TypedFactories;
+using FindAndBook.Data;
+using FindAndBook.Data.Contracts;
+using FindAndBook.Factories;
+using FindAndBook.Services;
+using FindAndBook.Services.Contracts;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FindAndBook.API.Startup))]
+
+namespace FindAndBook.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+
+            // Data Layer Bindings
+            builder.RegisterType<DbContext>()
+                .As<IDbContext>()
+                .InstancePerRequest();
+            builder.RegisterType(typeof(Repository<>))
+                    .As(typeof(IRepository<>))
+                    .InstancePerRequest();
+            builder.RegisterType<UnitOfWork>()
+                   .As<IUnitOfWork>()
+                   .InstancePerRequest();
+
+            // Service layer Bindings
+            builder.RegisterType<RestaurantsService>()
+                .As<IRestaurantsService>()
+                .InstancePerRequest();
+            builder.RegisterType<UsersService>()
+                .As<IUsersService>()
+                .InstancePerRequest();
+            builder.RegisterType<BookingsService>()
+                   .As<IBookingsService>()
+                   .InstancePerRequest();
+            builder.RegisterType<TablesService>()
+                .As<ITablesService>()
+                .InstancePerRequest();
+            builder.RegisterType<BookedTablesService>()
+                .As<IBookedTablesService>()
+                .InstancePerRequest();
+            builder.RegisterType<AuthService>()
+                .As<IAuthService>()
+                .InstancePerRequest();
+
+            // Factories
+            AutofacTypedFactoryExtensions.RegisterTypedFactory<IUsersFactory>(builder).ReturningConcreteType();
+            AutofacTypedFactoryExtensions.RegisterTypedFactory<IBookingsFactory>(builder).ReturningConcreteType();
+            AutofacTypedFactoryExtensions.RegisterTypedFactory<IRestaurantsFactory>(builder).ReturningConcreteType();
+            AutofacTypedFactoryExtensions.RegisterTypedFactory<IBookedTablesFactory>(builder).ReturningConcreteType();
+            AutofacTypedFactoryExtensions.RegisterTypedFactory<ITablesFactory>(builder).ReturningConcreteType();
+            AutofacTypedFactoryExtensions.RegisterTypedFactory<ITokensFactory>(builder).ReturningConcreteType();
+
+            var config = GlobalConfiguration.Configuration;
+            builder.RegisterWebApiFilterProvider(config);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
