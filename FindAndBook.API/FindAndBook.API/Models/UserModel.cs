c2663f04cdd7bf36d46ffbd2b0fcb1411e8cbc52
georgievayo <yo.ana.georgieva@abv.bg>
2018-06-20 15:11:35 +0300

    Add validation for login and register models.

--- FindAndBook.API/FindAndBook.API/Models/UserModel.cs
+++ FindAndBook.API/FindAndBook.API/Models/UserModel.cs
@@ -1,22 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FindAndBook.API.Models
 {
     public class UserModel
     {
+        [Required]
+        [MinLength(6)]
         public string Username { get; set; }
 
+        [Required]
+        [MinLength(6)]
+        [RegularExpression("[a-zA-Z]+[0-9]+", ErrorMessage = "Password must contain at least one letter and digit.")]
         public string Password { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [RegularExpression("[a-zA-Z]+", ErrorMessage = "First name must contain only letters.")]
         public string FirstName { get; set; }
 
+        [Required]
+        [RegularExpression("[a-zA-Z]+", ErrorMessage = "Last name must contain only letters.")]
         public string LastName { get; set; }
 
+        [Required]
+        [RegularExpression("[0-9]+", ErrorMessage = "Phone number must contain only digits.")]
         public string PhoneNumber { get; set; }
     }
 }
